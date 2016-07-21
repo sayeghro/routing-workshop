674b68606608c7a3738793a5be839d8d483379e5
Robert Sayegh <robert.sayegh@dali.dartmouth.edu>
2016-07-21 16:06:57 -0400

    done with basic routing starter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
 import './style.scss';
 
 // entry point that just renders app
 // could be used for routing at some point
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />
+  , document.getElementById('main'));
